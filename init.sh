7c92bd331a383d4c17f136c2ce698525e1b86d94
1735v <vovasapuzhak@gmail.com>
2020-04-21 18:59:08 +0300

    1.9 gunicorn commit4

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-available/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /web/etc/ngnix.conf /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 

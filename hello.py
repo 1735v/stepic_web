a92d2cdb72380055eb5fa89a8dc7394eb5bbb5a4
1735v <vovasapuzhak@gmail.com>
2020-04-21 17:11:31 +0300

    1.9 gunicorn commit3

--- hello.py
+++
@@ -1,8 +0,0 @@
-def application(env, start_response):
-    query_string=env['QUERI_STRING']
-    split_query_string=query_string.split("&")
-    body=[bytes(i+'\n','ascii') for i in split_query_string]
-    status='200 OK' 
-    headers=[('Content-Type','text/plain')]
-    start_response(status, headers)
-    return body
